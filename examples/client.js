14d178aeb27a00238509b3c2503bbd8ac8ddfd59
Dmitriy Tsvettsikh <dmitrycvet@gmail.com>
2019-05-07 00:58:24 +0500

    dtls example, see client.js

--- examples/client.js
+++ examples/client.js
@@ -1,8 +1,28 @@
 const coap  = require('../') // or coap
-    , req   = coap.request('coap://localhost/Matteo')
+const dtls = require('@nodertc/dtls');
+
+const socket = dtls.connect({
+  type: 'udp4',
+  remotePort: 8089,
+  remoteAddress: '127.0.0.1',
+  maxHandshakeRetransmissions: 3,
+  alpn: 'coap',
+  pskIdentity: 'coap',
+  pskSecret: 'deadbeaf',
+  cipherSuites: [
+    'TLS_PSK_WITH_AES_128_GCM_SHA256',
+    'TLS_PSK_WITH_AES_256_GCM_SHA384'
+  ]
+})
+
+const agent = new coap.Agent({ socket });
+
+const req = coap.request({ agent })
 
 req.on('response', function(res) {
   res.pipe(process.stdout)
+  
+  socket.close();
 })
 
 req.end()
